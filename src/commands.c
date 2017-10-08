cd3d25e4af6538993406cc76aedfcdd3aa2e457a
aeis <pyungryulahn1009@gmail.com>
2017-10-08 21:06:38 +0900

    hello

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,54 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "commands.h"
+#include <unistd.h>
+
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
-  return 0;
+  else if (chdir(argv[1]))
+ 
+   return 1;
+ 
+  else
+   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
+ 
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
-
-  return 0;
+  else
+ {  
+    char buf[2048];
+    getcwd(buf,8096);
+    printf("%s\n", buf);
+    return 0;
+ }
+  
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+
+  if ((argc==2) && (strcmp(argv[0],"cd")==0))
+	return 1;
+
+  else
+	return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+ 
+  if (argc >1)
+     return 0;
+
+  else if (strcmp(argv[0], "pwd")!=0)
+     return 0;
+  else 
+     return 1;
 }
